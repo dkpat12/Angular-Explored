e2f81eb96cc2ed22f1c8859f050444106fe33dc8
Dillion Patrick <dkpat12@gmail.com>
2015-02-11 12:01:16 -0500

    IT WORKS!
    not very pretty but it works!

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-$(function() {
-    var app = angular.module('webGallery', []);
+(function(){
+    var app = angular.module('tutorialGallery', []);
 
-    app.controller('GalleryController', ['$http', function($http) {
+    app.controller('GallController', ['$http', function($http) {
         var gallery = this;
         gallery.items = [];
-        $http.get('/tutorials.json').success(function(data) {
+        console.log("Gallery Controller was called!");
+        $http.get('tutorials.json').success(function(data) {
             gallery.items = data;
+            console.log("We attempted to success log the info!");
         });
-    }]);
-
-});
+    }]); 
+})();
